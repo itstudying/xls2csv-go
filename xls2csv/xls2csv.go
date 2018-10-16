5ab0ec3c2da2b8a3ea5c849fd1758c708296ab02
lijq <itstudying@hotmail.com>
2018-10-16 16:58:45 +0800

    加入libxls库文件，

--- xls2csv/xls2csv.go
+++ xls2csv/xls2csv.go
@@ -1,8 +1,9 @@
 package xls2csv
 
 /*
-#cgo CFLAGS: -I/usr/local/libxls/include
+#cgo darwin CFLAGS: -I${SRCDIR}/libxls_darwin/include
 #cgo darwin LDFLAGS: -L${SRCDIR}/libxls_darwin/lib -l xlsreader
+#cgo linux CFLAGS: -I${SRCDIR}/libxls_linux/include
 #cgo linux LDFLAGS: -L${SRCDIR}/libxls_linux/lib -l xlsreader
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,22 +13,12 @@ package xls2csv
 import "C"
 
 import (
-	"encoding/csv"
 	"fmt"
-	"strings"
 	"unsafe"
 )
 
-// XLS2CSV converts XLS file to CSV records.
-//     Params:
-//       xlsFile: XLS file name.
-//       sheetID: sheet ID to be converted. It's 0-based.
-//     Return:
-//       records: CSV records. Each record is a slice of fields.
-//                See https://godoc.org/encoding/csv#Reader.ReadAll for more info.
-func XLS2CSV(xlsFile string, sheetID int) (records [][]string, err error) {
+func XLS2String(xlsFile string, sheetID int) (records string, err error) {
 	var buf *C.char
-	var r *csv.Reader
 
 	f := C.CString(xlsFile)
 	// C string should be free after use.
@@ -35,24 +26,15 @@ func XLS2CSV(xlsFile string, sheetID int) (records [][]string, err error) {
 
 	id := C.int(sheetID)
 
-	// xls2csv() will return a buffer(char *) contains CSV string.
-	// The buffer should be free in C.
 	buf = C.xls2csv(f, id)
 	if buf == nil {
 		err = fmt.Errorf("xls2csv() error")
-		goto end
+		return records, nil
 	}
 
 	// Free memory block after use.
 	defer C.free(unsafe.Pointer(buf))
 
-	r = csv.NewReader(strings.NewReader(C.GoString(buf)))
-	records, err = r.ReadAll()
-
-end:
-	if err != nil {
-		return [][]string{}, err
-	}
-
+	records = C.GoString(buf)
 	return records, nil
 }
